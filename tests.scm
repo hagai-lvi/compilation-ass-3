065c55cc0b3733ec734c9cb259cf1b76b8e0fe92
hagai-lvi <hagai.lvi@gmail.com>
2014-12-10 20:08:10 +0200

    added tests framework

---
+++ tests.scm
@@ -0,0 +1,44 @@
+;;; Copyright (c) 2012 Andrew W. Keep
+;;; See the accompanying file Copyright for detatils
+;(load "formatter.scm")
+(import
+	(rnrs)
+	(rough-draft unit-test)
+	(rough-draft console-test-runner))
+
+(define-test-suite foo
+
+	(define-test test-mock
+		(assert-equal? 1 1)
+	)
+
+)
+
+;(run-test-suites foo)
+;(run-test foo first-test)
+;(run-tests foo test-one)
+
+
+(exit (run-test-suites foo))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<string>
